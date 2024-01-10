eaa45ab8dd2b7f268971889c508cde44d8624e11
codef101 <carlbiwott@gmail.com>
2024-01-10 11:40:31 +0300

    Fix: Footer

--- ci4/app/Views/themes/frontend/startbootstrap/main/footer.php
+++ ci4/app/Views/themes/frontend/startbootstrap/main/footer.php
@@ -1,116 +1,119 @@
- <footer role="contentinfo" class="bottom">
-     <div>
-         <div class="section">
-             <div class="new"></div>
-         </div>
-         <div class="iparys_inherited">
-             <div class="iparsys parsys">
-                 <div class="referenceComponent reference parbase section">
-                     <div class="cq-dd-paragraph">
-                         <div class="footernavigationcomp footerNavigationComponent parbase">
-                             <div class="page-footer">
-                                 <div class="container">
-                                     <div class="page-footer_content">
-                                         <div class="page-footer_legal">
-                                             <p>© Copyright 2023 Travsify Global Inc.¦ &nbsp;<a href="/company/legal/intellectual/" adhocenable="false">All
-                                                     rights reserved</a>. | 635 Pine Ave, Pacific Grove, California,
-                                                 U.S.A. |  +12134686786 </p>
-                                         </div>
-                                         <nav class="page-footer_links mobile-display">
-                                             <ul class="page-footer_links_list">
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="https://www.salesforce.com/company/legal/">
-                                                         Legal
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="/company/legal/sfdc-website-terms-of-service/">
-                                                         Terms of Service
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="/company/privacy/">
-                                                         Privacy Information
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="https://www.salesforce.com/company/disclosure/">
-                                                         Responsible Disclosure
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="https://trust.salesforce.com/en/" target="_blank">
-                                                         Trust
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="https://www.salesforce.com/company/contact-us/?d=cta-glob-footer-11">
-                                                         Contact
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link optanon-toggle-display" href="#" data-ignore-geolocation="true">
-                                                         Cookie Preferences
-                                                     </a>
-                                                 </li>
-                                                 <li class="page-footer_links_item">
-                                                     <a class="page-footer_link " href="/form/other/privacy-request/?d=cta-footer-1">
-                                                         <img alt="" class="footer-link-icon footer-link-icon_left" src="../assets2/images/icon-cpra.svg">Your Privacy
-                                                         Choices
-                                                     </a>
-                                                 </li>
-                                             </ul>
-                                         </nav>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="referenceComponent reference parbase section">
-                     <div class="cq-dd-paragraph">
-                         <div class="dockedcontainer dockedContainer parbase">
-                             <div class="docked-container  margin-20-right-lg fixed">
-                                 <div class="fixedFooterCTAItemComponent parbase section">
-                                 </div>
-                                 <div class="fixedFooterCTAItemComponent parbase section">
-                                 </div>
-                                 <div class="randomImageComponent list parbase section">
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
- </footer>
+<link rel="stylesheet" href="../assets2/css/all.bundle.d787c023a5b975d5b19d.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="../assets2/css/webpack-style-manifest-formContainerV2-js.bundle.09269c0ac020a504a901.css">
 
- <div class="sf-call">
-     <div class="call-btn-wrapper">
-         <a class="btn" href="tel:0800 020 0431" aria-label="Contact, Telefoonnummer"></a>
-     </div>
- </div>
+<footer role="contentinfo" class="bottom">
+    <div>
+        <div class="section">
+            <div class="new"></div>
+        </div>
+        <div class="iparys_inherited">
+            <div class="iparsys parsys">
+                <div class="referenceComponent reference parbase section">
+                    <div class="cq-dd-paragraph">
+                        <div class="footernavigationcomp footerNavigationComponent parbase">
+                            <div class="page-footer">
+                                <div class="container">
+                                    <div class="page-footer_content">
+                                        <div class="page-footer_legal">
+                                            <p>© Copyright 2023 Travsify Global Inc.¦ &nbsp;<a href="/company/legal/intellectual/" adhocenable="false">All
+                                                    rights reserved</a>. | 635 Pine Ave, Pacific Grove, California,
+                                                U.S.A. |  +12134686786 </p>
+                                        </div>
+                                        <nav class="page-footer_links mobile-display">
+                                            <ul class="page-footer_links_list">
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="https://www.salesforce.com/company/legal/">
+                                                        Legal
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="/company/legal/sfdc-website-terms-of-service/">
+                                                        Terms of Service
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="/company/privacy/">
+                                                        Privacy Information
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="https://www.salesforce.com/company/disclosure/">
+                                                        Responsible Disclosure
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="https://trust.salesforce.com/en/" target="_blank">
+                                                        Trust
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="https://www.salesforce.com/company/contact-us/?d=cta-glob-footer-11">
+                                                        Contact
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link optanon-toggle-display" href="#" data-ignore-geolocation="true">
+                                                        Cookie Preferences
+                                                    </a>
+                                                </li>
+                                                <li class="page-footer_links_item">
+                                                    <a class="page-footer_link " href="/form/other/privacy-request/?d=cta-footer-1">
+                                                        <img alt="" class="footer-link-icon footer-link-icon_left" src="../assets2/images/icon-cpra.svg">Your Privacy
+                                                        Choices
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="referenceComponent reference parbase section">
+                    <div class="cq-dd-paragraph">
+                        <div class="dockedcontainer dockedContainer parbase">
+                            <div class="docked-container  margin-20-right-lg fixed">
+                                <div class="fixedFooterCTAItemComponent parbase section">
+                                </div>
+                                <div class="fixedFooterCTAItemComponent parbase section">
+                                </div>
+                                <div class="randomImageComponent list parbase section">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
- <script type="text/javascript">
-     window.document.dispatchEvent(
-         new CustomEvent('www_track', {
-             detail: {
-                 event: 'custEv_pageDataAvailable',
-                 templateName: '',
-                 templateId: '',
-                 taxonomy: JSON.parse('{"pageTagsManual":[],"pageTagsAuto":[]}'),
-                 experiment: {
-                     'optimizelyExp': '',
-                     'optimizelyVar': ''
-                 }
-             },
-         })
-     );
-     //});
- </script>
- <script defer="" src="assets2/js/chat.js"></script>
- <script defer="" src="assets2/js/tableau-2.9.2.min.js"></script>
- </body>
+<div class="sf-call">
+    <div class="call-btn-wrapper">
+        <a class="btn" href="tel:0800 020 0431" aria-label="Contact, Telefoonnummer"></a>
+    </div>
+</div>
 
- </html>
+<script type="text/javascript">
+    window.document.dispatchEvent(
+        new CustomEvent('www_track', {
+            detail: {
+                event: 'custEv_pageDataAvailable',
+                templateName: '',
+                templateId: '',
+                taxonomy: JSON.parse('{"pageTagsManual":[],"pageTagsAuto":[]}'),
+                experiment: {
+                    'optimizelyExp': '',
+                    'optimizelyVar': ''
+                }
+            },
+        })
+    );
+    //});
+</script>
+<script defer="" src="assets2/js/chat.js"></script>
+<script defer="" src="assets2/js/tableau-2.9.2.min.js"></script>
+</body>
+
+</html>
